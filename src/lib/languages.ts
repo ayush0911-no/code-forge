abb794891a5ae4f77a1209eba111aec048ae1ac2
Ayush singh <ayushkrsingh204@gmail.com>
2025-09-23 08:31:09 +0000

    Remove some languages Such as GO,Rust,C#,Typescript,HTML,CSS

--- src/lib/languages.ts
+++ src/lib/languages.ts
@@ -7,11 +7,5 @@ export type Language = {
 export const languages: Language[] = [
   { value: 'javascript', label: 'JavaScript', extension: 'js' },
   { value: 'python', label: 'Python', extension: 'py' },
-  { value: 'html', label: 'HTML', extension: 'html' },
-  { value: 'css',label: 'CSS', extension: 'css' },
-  { value: 'typescript', label: 'TypeScript', extension: 'ts' },
   { value: 'java', label: 'Java', extension: 'java' },
-  { value: 'go', label: 'Go', extension: 'go' },
-  { value: 'rust', label: 'Rust', extension: 'rs' },
-  { value: 'csharp', label: 'C#', extension: 'cs'},
 ];
